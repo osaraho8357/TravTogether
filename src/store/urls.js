a35b88032a5b8bbdf08a3d35fc57f6d1428fad21
sebbyu <you.sebastian.1.618@gmail.com>
2021-03-24 00:06:06 -0700

    production

--- src/store/urls.js
+++ src/store/urls.js
@@ -1,8 +1,8 @@
 
 
 
-export const BASEURL = "http://127.0.0.1:8000"
-export const BASEHOST = "127.0.0.1:8000"
-// export const BASEURL = "https://travtogether-server.herokuapp.com";
-// export const BASEHOST = "travtogether-server.herokuapp.com"
+// export const BASEURL = "http://127.0.0.1:8000"
+// export const BASEHOST = "127.0.0.1:8000"
+export const BASEURL = "https://travtogether-server.herokuapp.com";
+export const BASEHOST = "travtogether-server.herokuapp.com"
 export const DEFAULT_PHOTO_URL = "https://i.pinimg.com/474x/f0/7c/8d/f07c8d1542e4338c31cadae42bb102dc.jpg"
